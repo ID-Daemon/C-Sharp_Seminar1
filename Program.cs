ccdf1a036304f1bb0a07f2f42a81afd683074b48
Neuf eGO <patronus1994@gmail.com>
2022-10-07 10:00:45 +0500

    Add *.cs file

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿
+// Первое задание
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+// int b = a * a;
+int squaredNumber = Convert.ToInt32(Math.Pow(number, 2));
+
+Console.WriteLine("Квадрат заданного числа = "  + squaredNumber);
+
+// Второе задание
+Console.WriteLine("Введите номер дня недели");
+int numberWeakDay = int.Parse(Console.ReadLine()!);
+
+switch (numberWeakDay)
+{
+    case 1: Console.WriteLine("Понедельник");
+            break;
+    case 2: Console.WriteLine("Вторник");
+            break;
+    case 3: Console.WriteLine("Среда");
+            break;
+    case 4: Console.WriteLine("Четверг");
+            break;
+    case 5: Console.WriteLine("Пятница");
+            break;
+    case 6: Console.WriteLine("Суббота");
+            break;
+    case 7: Console.WriteLine("Воскресенье");
+            break;
+    default: Console.WriteLine("В недели всего 7 дней");
+            break;
+}
+
+
+
+// Третье задание
+Console.WriteLine("Введи число");
+
+int n = int.Parse(Console.ReadLine()!);
+
+int i;
+if (n > 0) {
+
+    i = n - n - n;  
+
+} else {
+    i = n;
+    n = n - n - n;
+}
+
+Console.Write("\"");
+while (i<n)
+{
+    Console.Write($"{i}, ");
+    i++;
+}
+Console.Write(i);
+Console.Write("\"");
